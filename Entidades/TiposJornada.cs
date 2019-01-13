6bdecc4b8e29098b35108d81d3b6c022beaeb39b
YordyLeonidas <sisuke.uchiha97@gmail.com>
2019-01-12 22:35:33 -0500

    Creando otros objetos para nuestra escuela

---
+++ Entidades/TiposJornada.cs
@@ -0,0 +1,5 @@
+namespace CoreEscuela.Entidades{
+    public enum TiposJornada{
+        Mañana, Tarde, Noche
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,12 +7,26 @@ namespace Etapa1
     {
         static void Main(string[] args)
         {
-            
             var escuela = new Escuela("Nuevo Milenio", 2000, TiposEscuela.Secundaria, pais: "Perú");
             escuela.Ciudad = "Trujillo";
             //escuela.Pais = "Perú";
             //escuela.TipoEscuela = TiposEscuela.Secundaria;
             Console.WriteLine(escuela);
+
+            var curso1 = new Curso(){
+                Nombre = "FUNPRO"
+            };
+            var curso2 = new Curso(){
+                Nombre = "FUNAL"
+            };
+            var curso3 = new Curso(){
+                Nombre = "ESTAD"
+            };
+            System.Console.WriteLine("====================================");
+            System.Console.WriteLine($"Nombre: {curso1.Nombre} -> {curso1.UniqueId}");
+            System.Console.WriteLine($"Nombre: {curso2.Nombre} -> {curso2.UniqueId}");
+            System.Console.WriteLine($"Nombre: {curso3.Nombre} -> {curso3.UniqueId}");
+
         }
     }
 }
